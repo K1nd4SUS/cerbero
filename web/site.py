48faa6010192f1f9da8bf11066e9f1899f266a9e
M4RC02U1F4A4 <23193188+M4RC02U1F4A4@users.noreply.github.com>
2021-11-23 00:29:18 +0100

    Added JSON update #7

--- web/site.py
+++ web/site.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, request, redirect
 import json
 
 app = Flask(__name__)
 
 service = """
-    <div class="col m-4 serviceCard p-3">
+<div class="col m-4 serviceCard p-3">
 	<div class="row text-center">
 		<p class="fw-bold">##NAME##</p>
 	</div>
@@ -23,8 +23,9 @@ service = """
 		##RULE##
 	</ul>
 	<div class="row">
-		<form action="">
-			<input type="text" name="" id="s1" placeholder="New rule">
+		<form action="/jsonUpdater/##NAME##" method="POST">
+			<input type="text" name="regex" placeholder="New rule">
+            <input type="submit" value="update">
 		</form>
 	</div>
 </div>
@@ -40,7 +41,7 @@ def hello():
             mode = "BLACKLIST"
         elif k['mode'] == "w":
             mode = "WHITELIST"
-        services += service.replace("##NAME##", k['name']).replace("##PROTOCOL##", k['protocol'].upper()).replace("##PORT##", str(k['dport'])).replace("##MODE##", mode)
+        services += service.replace("##NAME##", k['name'].replace(" ", "")).replace("##PROTOCOL##", k['protocol'].upper()).replace("##PORT##", str(k['dport'])).replace("##MODE##", mode)
         rule = ""
         for r in k['regexList']:
             rule += f"<li>{r[1:-1]}</li>"
@@ -48,7 +49,33 @@ def hello():
 
     return render_template('index.html', services=services)
     
+@app.route('/jsonUpdater/<path:service>', methods=['POST'])
+def jsonUpdate(service):
+    regex = "(" + request.form.get('regex') + ")"
+    with open("../config.json", "r") as f:
+        data = json.load(f)
+    
+    # find the right service
+    for k in range(len(data['services'])):
+        if data['services'][k]['name'].replace(" ", "") == service:
+            index = k
+
+    # check if the rule already exist, in this case it need to be removed
+    # otherwise we can add the new rule to the file
+    for rule in data['services'][index]['regexList']:
+        if(rule == regex):
+            #remove
+            data['services'][index]['regexList'].remove(rule)
+            with open('../config.json', 'w') as json_file:
+                json.dump(data, json_file)
+                return redirect("/")
+
+    data['services'][index]['regexList'].append(regex)
+    with open('../config.json', 'w') as json_file:
+        json.dump(data, json_file)
+    return redirect("/")
+
 
 
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=8080)
