746edcfc78fe0fc2be6b154cdb44d8c3b750b820
tiz314 <63679072+tiz314@users.noreply.github.com>
2021-11-24 21:56:35 +0100

    Fix references and js function
    Close #8

--- web/static/js/comm.js
+++ web/static/js/comm.js
@@ -22,20 +22,3 @@ function checkUpdate()
         } 
     };
 }
-
-
-function send_rules(servURL, params, method) {
-    method = method || "post";
-    var form = document.createElement("form");
-    form.setAttribute("method", method);
-    form.setAttribute("action", servURL);
-    for(var key in params) {
-        var hiddenField = document.createElement("input");
-        hiddenField.setAttribute("type", "hidden");
-        hiddenField.setAttribute("name", key);
-        hiddenField.setAttribute("value", params[key]);
-        form.appendChild(hiddenField);
-    }
-    document.body.appendChild(form);
-    form.submit();
-}
