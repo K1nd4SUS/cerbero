2d7c4bee530e2deb24bee009fe557f6af914b1f1
Mattia Perfumo <perfumo.m10@gmail.com>
2023-08-29 17:52:02 +0200

    branch cleaning

--- cerbero2.go
+++
@@ -1,3 +0,0 @@
-/*
-	Questa sarà la seconda versione di Cerbero, riscritta da capo per varie ragioni
-*/
--- go.mod
+++
@@ -1,5 +0,0 @@
-module starting
-
-go 1.17
-
-require github.com/florianl/go-nfqueue v1.3.0 // indirect
