56c9bcd28c8d03c88f20af0a6f9c8328226cadde
Francesco Aliprandi <aliprandifrancescopp@gmail.com>
2024-03-07 14:24:46 +0100

    feat(components): create `SidebarItem` component

--- web/frontend/src/components/Sidebar.tsx
+++ web/frontend/src/components/Sidebar.tsx
@@ -1,10 +1,18 @@
-import { Button } from "@nextui-org/react"
+import { Button, Divider } from "@nextui-org/react"
 import { motion } from "framer-motion"
 import { useState } from "react"
-import { useNavigate } from "react-router-dom"
-import { FaBars, FaHouse } from "react-icons/fa6"
+import { Link, useNavigate } from "react-router-dom"
+import { FaBars, FaCode, FaHouse } from "react-icons/fa6"
+import { useFetchSync } from "../hooks/useFetch"
+import { CerberoService } from "../types/cerbero"
+import SidebarItem from "./SidebarItem"
 
 export default function Sidebar() {
+  const [
+    servicesResponse,
+    ,
+  ] = useFetchSync<CerberoService[]>("/api/services")
+
   const [isOpen, setIsOpen] = useState(false)
   const navigate = useNavigate()
 
@@ -23,7 +31,24 @@ export default function Sidebar() {
             <FaBars/>
           </Button>}
       </div>
-      <div className="h-full"></div>
+      <div className="h-full w-full flex flex-col items-center gap-2 p-4 m-2 border border-default-200 rounded-lg">
+        <SidebarItem icon={<FaCode/>} isSidebarOpen={isOpen} link="/services" name="Services">
+          {servicesResponse?.map(service => {
+            return (
+              <Link key={service.nfq} to={`/services/${service.nfq}`} className="px-4 py-1 rounded-lg bg-default-200 hover:opacity-75 border border-default-300">
+                <li className="flex items-center gap-2">
+                  <span className="text-sm">{service.name}</span>
+                  <div className="flex items-center gap-2 ml-auto">
+                    <span className="font-mono text-xs">nfq:{service.nfq}</span>
+                    <span className="font-mono text-xs text-success">{service.port}</span>
+                  </div>
+                </li>
+              </Link>
+            )
+          })}
+        </SidebarItem>
+        <Divider/>
+      </div>
       <div className="w-full flex items-center justify-center">
         {isOpen ?
           <Button variant="bordered" className="w-full flex items-center gap-4" onPress={() => navigate("/")}>
